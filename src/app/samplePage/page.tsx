7b1cf60a60e9863928f2efaa1536ab78449b6bde
김학준 <khjun@gimhagjun-ui-noteubug.local>
2024-10-02 03:08:13 +0900

    build error fix7

--- src/app/samplePage/page.tsx
+++ src/app/samplePage/page.tsx
@@ -7,7 +7,7 @@ export default async function DetailPage() {
     `https://claying.shop/briefing/top_videos/HULbCjTw3tw`
   );
   const data = await response.json();
-  const detailData: DataProps | null = data[0] || null;
+  const detailData: DataProps = data[0] || null;
 
   return <ClientSide detailData={detailData} id="HULbCjTw3tw" />;
 }
