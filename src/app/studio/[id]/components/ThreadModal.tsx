662d045eec10455b8dacc5d83275ef4d80edb83c
bluemeberg <rlagrkwns30@naver.com>
2025-02-19 20:21:49 +0900

    250215 feat. studio overview 추가 + fix. api

--- src/app/studio/[id]/components/ThreadModal.tsx
+++ src/app/studio/[id]/components/ThreadModal.tsx
@@ -14,8 +14,8 @@ interface ThreadContent {
   content: string;
 }
 
-const NEXT_PUBLIC_API_BASE_URL = "http://0.0.0.0:8000";
-// const NEXT_PUBLIC_API_BASE_URL = "https://youticle.shop";
+// const NEXT_PUBLIC_API_BASE_URL = "http://0.0.0.0:8000";
+const NEXT_PUBLIC_API_BASE_URL = "https://youticle.shop";
 
 export default function ThreadModal({
   videoId,
